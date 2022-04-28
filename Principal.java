bc3875f7a9cf7d9df646b5b485c07540f06fd644
leetycia <103155758+leetycia@users.noreply.github.com>
2022-04-28 19:52:06 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,9 @@
+package principal;
+public class Principal {
+    public static void main(String[] args) {
+        int num = 10;
+        NovoFat fat = new NovoFat();
+        System.out.println("Factorial:" +fat.factorialResult(num));
+    }
+        
+    }
